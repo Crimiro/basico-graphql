d1f1f061c2f74b968051d9f96424be9e6648f02d
Christian Michael Rosales <crimiro05@gmail.com>
2021-02-08 18:20:27 -0600

    Resolvers

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const { graphql, buildSchema } = require('graphql');
 //Definiendo el schema inicial
 const schema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    saludo: String
   }
 `)
 
+// Configurar los resolvers
+const resolvers = {
+  hello: () => {
+    return 'Hola Mundo!'
+  },
+  saludo: () => {
+    return 'Hola a todos'
+  }
+}
+
 // Ejecutar el query
 // Se envia el schema y el argumento a ejecutar
-graphql(schema, '{ hello }').then((data) => {
+graphql(schema, '{ hello, saludo }', resolvers).then((data) => {
   console.log(data);
 })
